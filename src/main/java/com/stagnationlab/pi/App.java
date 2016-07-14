3db8466d317b8aaa828d52d29188e6760d2b9856
Priit Kallas <kallaspriit@gmail.com>
2016-07-14 17:15:39 +0300

    Implemented DAC example.

--- src/main/java/com/stagnationlab/pi/App.java
+++ src/main/java/com/stagnationlab/pi/App.java
@@ -3,35 +3,68 @@ package com.stagnationlab.pi;
 import com.pi4j.io.gpio.*;
 import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
+import com.pi4j.io.i2c.I2CBus;
+import com.pi4j.io.i2c.I2CDevice;
+import com.pi4j.io.i2c.I2CFactory;
 import com.pi4j.system.NetworkInfo;
 import com.pi4j.system.SystemInfo;
+import com.pi4j.wiringpi.SoftPwm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.Console;
 import java.io.IOException;
 import java.text.ParseException;
+
+import static com.pi4j.wiringpi.Gpio.wiringPiSetup;
 
 public class App {
 
     private static Logger log = LoggerFactory.getLogger(App.class);
 
-    private final Pin LED_PIN = RaspiPin.GPIO_09;
+    private final Pin RED_LED_PIN = RaspiPin.GPIO_22;
+    private final Pin YELLOW_LED_PIN = RaspiPin.GPIO_23;
+    private final Pin GREEN_LED_PIN = RaspiPin.GPIO_29;
     private final Pin BUTTON_PIN = RaspiPin.GPIO_21;
+    private final Pin MOTION_PIN = RaspiPin.GPIO_00;
 
     private final GpioController gpio = GpioFactory.getInstance();
-    private final GpioPinDigitalOutput ledPin = gpio.provisionDigitalOutputPin(LED_PIN, PinState.LOW);
+    private final GpioPinDigitalOutput redLedPin = gpio.provisionDigitalOutputPin(RED_LED_PIN, PinState.LOW);
+    //private final GpioPinAnalogOutput yellowLedPin = gpio.provisionAnalogOutputPin(YELLOW_LED_PIN, 0);
+    private final GpioPinDigitalOutput greenLedPin = gpio.provisionDigitalOutputPin(GREEN_LED_PIN, PinState.LOW);
     private final GpioPinDigitalInput buttonPin = gpio.provisionDigitalInputPin(BUTTON_PIN, PinPullResistance.PULL_UP);
+    private final GpioPinDigitalInput motionPin = gpio.provisionDigitalInputPin(MOTION_PIN, PinPullResistance.PULL_DOWN);
 
-    private int buttonPressCount = 0;
+    private boolean isRunning = false;
 
     private App() throws InterruptedException, ParseException, IOException {
         log.info("starting experiments");
 
+        isRunning = true;
+
         showSystemInfo();
         showSetupInfo();
-        testLed();
-        testButton();
 
+        setupButtonListener();
+        setupMotionListener();
+
+        testLed();
+
+        try {
+            testADC();
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            log.warn("testing i2c adc device failed (" + e.getMessage() + ")");
+        }
+
+        testDAC();
+
+        waitForQuit();
+
+        isRunning = false;
+
+        //testLed();
         gpio.shutdown();
 
         log.info("shutdown successful");
@@ -40,30 +73,78 @@ public class App {
     private void testLed() throws InterruptedException {
         log.info("-- testing led --");
 
-        ledPin.setShutdownOptions(true, PinState.LOW);
+        redLedPin.setShutdownOptions(true, PinState.LOW);
 
-        ledPin.high();
+        greenLedPin.high();
         log.info("> ON");
         Thread.sleep(1000);
-
-        ledPin.low();
+        greenLedPin.low();
         log.info("> OFF");
-        Thread.sleep(1000);
-
-        ledPin.toggle();
-        log.info("> ON");
-        Thread.sleep(1000);
-
-        ledPin.toggle();
-        log.info("> OFF");
-        Thread.sleep(1000);
-
-        log.info("> ON for a second");
-        ledPin.pulse(1000, true); // set second argument to 'true' use a blocking call
     }
 
-    private void testButton() throws InterruptedException {
-        log.info("-- testing button for three button presses --");
+    private void testADC() throws IOException, I2CFactory.UnsupportedBusNumberException, InterruptedException {
+        I2CBus bus = I2CFactory.getInstance(I2CBus.BUS_1);
+        I2CDevice device = bus.getDevice(0x55);
+
+        device.write(0x02, (byte)0x20);
+
+        Thread.sleep(500);
+
+        (new Thread(() -> {
+            while (isRunning) {
+                byte[] data = new byte[2];
+                try {
+                    device.read(0x00, data, 0, 2);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                // convert the data to 12-bits
+                int value = ((data[0] & 0x0F) * 256 + (data[1] & 0xFF));
+
+                log.info("digital value of analog input: " + value);
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        })).start();
+    }
+
+    private void testDAC() {
+        final int pin = 23;
+        final int max = 100;
+
+        SoftPwm.softPwmCreate(pin, 0, max);
+
+        (new Thread(() -> {
+            float value = 0.0f;
+            float step = 0.5f;
+
+            while (isRunning) {
+                //yellowLedPin.setValue(value);
+
+                SoftPwm.softPwmWrite(pin, Math.round(value));
+
+                if (value + step > max || value + step < 0) {
+                    step *= -1.0f;
+                }
+
+                value += step;
+
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        })).start();
+    }
+
+    private void setupButtonListener() throws InterruptedException {
+        log.info("setting up button listener on pin " + buttonPin.getName());
 
         buttonPin.addListener(new GpioPinListenerDigital() {
             @Override
@@ -71,28 +152,37 @@ public class App {
                 if (event.getState() == PinState.LOW) {
                     log.info("button pressed");
 
-                    buttonPressCount++;
-
-                    ledPin.high();
+                    greenLedPin.high();
                 } else {
                     log.info("button released");
 
-                    ledPin.low();
+                    greenLedPin.low();
                 }
             }
         });
+    }
 
-        while (buttonPressCount < 3) {
-            Thread.sleep(500);
-        }
+    private void setupMotionListener() throws InterruptedException {
+        log.info("setting up motion listener on pin " + motionPin.getName());
 
-        log.info("button was pressed for three times, stopping listening");
+        motionPin.addListener(new GpioPinListenerDigital() {
+            @Override
+            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+                if (event.getState() == PinState.HIGH) {
+                    log.info("motion detected");
 
-        buttonPin.removeAllListeners();
+                    redLedPin.high();
+                } else {
+                    log.info("motion reset");
+
+                    redLedPin.low();
+                }
+            }
+        });
     }
 
     private void showSetupInfo() {
-        log.info("Led pin: " + LED_PIN.getName());
+        log.info("Led pin: " + YELLOW_LED_PIN.getName());
     }
 
     private void showSystemInfo() throws IOException, InterruptedException, ParseException {
@@ -178,6 +268,18 @@ public class App {
         log.info("VEC Frequency     :  " + SystemInfo.getClockFrequencyVEC());
         log.info("HDMI Frequency    :  " + SystemInfo.getClockFrequencyHDMI());
         log.info("DPI Frequency     :  " + SystemInfo.getClockFrequencyDPI());
+    }
+
+    private void waitForQuit() {
+        Console console = System.console();
+
+        if (console != null) {
+            console.format("\nPress Q to quit.\n");
+
+            String input = console.readLine();
+
+            log.info("entered: " + input);
+        }
     }
 
     public static void main(String[] args) throws InterruptedException, IOException, ParseException {
